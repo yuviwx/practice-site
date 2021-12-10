09e47e493d341716591c1c58846541ea549db002
yuviwx <yuviwx@gmail.com>
2021-12-10 21:43:20 +0200

    new directories + event listener to register

---
+++ js/pt_script.js
@@ -0,0 +1,8 @@
+let eventTarget = document.getElementsByClassName("register")
+
+let nav = () => {
+	window.location.href='./register form.html';
+}
+
+eventTarget[0].addEventListener('click', nav);
+eventTarget[1].addEventListener('click', nav);
