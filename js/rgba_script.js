cd13eba9dadef6ff6e922d288edf93f30a1282a2
yuviwx <yuviwx@gmail.com>
2021-12-13 01:53:00 +0200

    color picker without copy to board option

--- js/rgba_script.js
+++ js/rgba_script.js
@@ -1,21 +1,26 @@
 let range = document.querySelectorAll("input[type=range]");
 let value_box = document.getElementsByClassName("value_box");
-console.log(range, value_box)
+
+let rgba_letters = document.querySelectorAll(".num");
+let color = document.querySelector("#output_color");
 
 let onchange = (element, value) => {
     console.log(value)
     element.value = value;
+    color.style.backgroundColor = document.querySelector("#output_rgba").innerText;
 }
 
-for(let i=0; i<3; i++){
-    range[i].addEventListener("change", function(){
+for(let i=0; i<4; i++){
+    range[i].addEventListener("input", function(){
         let element = value_box[i];
         let value = range[i].value;
+        rgba_letters[i].innerHTML = value;
         onchange(element,value)
     });
     value_box[i].addEventListener("change", function(){
         let element = range[i];
         let value = value_box[i].value;
+        rgba_letters[i].innerHTML = value;
         onchange(element,value)
     });
 }
