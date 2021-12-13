701e03b88f3689aca514996f476ea0bd963897c5
yuviwx <yuviwx@gmail.com>
2021-12-13 12:17:24 +0200

    copy button and last color picker changes

--- js/rgba_script.js
+++ js/rgba_script.js
@@ -1,15 +1,18 @@
+// all the range input and the number input in two lists
 let range = document.querySelectorAll("input[type=range]");
 let value_box = document.getElementsByClassName("value_box");
 
+//the output text and the color box element's
 let rgba_letters = document.querySelectorAll(".num");
 let color = document.querySelector("#output_color");
 
+//adjustment function for all the elements(when one changes the others will change as well)
 let onchange = (element, value) => {
-    console.log(value)
     element.value = value;
     color.style.backgroundColor = document.querySelector("#output_rgba").innerText;
 }
 
+//event listener for the colors input elements
 for(let i=0; i<4; i++){
     range[i].addEventListener("input", function(){
         let element = value_box[i];
@@ -25,3 +28,18 @@ for(let i=0; i<4; i++){
     });
 }
 
+//copy to clipboard function with the rgba product
+function copy() {
+    /* Get the text field*/
+    const text = document.getElementById("output_rgba").innerText;
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(text);
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + text);
+  }
+
+//copy button event listener
+const copy_button = document.querySelector("#copy");
+copy_button.addEventListener('click', copy);
